404209c84c1d7a0852f10a803cefeea65fb5c4c5
adamkrieger <adamkrieger.ca@gmail.com>
2017-02-09 13:32:45 -0600

    Manually pointing at the upload script.

--- deploysite.sh
+++ deploysite.sh
@@ -3,4 +3,4 @@ HERE=`pwd`
 docker run -v $HERE:/usr/vol --rm --name boto3_instance \
     -e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    akrieger/boto3 python upload_files.py --bucket $TARGET_BUCKET --dir ./output
+    akrieger/boto3 python /usr/src/upload_files.py --bucket $TARGET_BUCKET --dir ./output
