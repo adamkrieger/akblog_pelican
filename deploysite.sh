ff58c56d298e7284b95679c444e868dc82d8352c
adamkrieger <adamkrieger.ca@gmail.com>
2017-02-09 13:22:43 -0600

    Correctly using the host machine connected to the volume.

--- deploysite.sh
+++ deploysite.sh
@@ -1,6 +1,6 @@
 HERE=`pwd`
 
-docker run -v $HERE --rm --name boto3_instance \
+docker run -v $HERE:/usr/vol --rm --name boto3_instance \
     -e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     akrieger/boto3 python upload_files.py --bucket $TARGET_BUCKET --dir ./output
