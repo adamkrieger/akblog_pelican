f2b754bb58e3379b86c582942a861ded1a155fa4
adamkrieger <adamkrieger.ca@gmail.com>
2017-02-09 11:07:06 -0600

    Redirected deploysite at the target bucket.

--- deploysite.sh
+++ deploysite.sh
@@ -3,4 +3,4 @@ HERE=`pwd`
 docker run -v $HERE --rm --name boto3_instance \
     -e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    akrieger/boto3
+    akrieger/boto3 python upload_files.py --bucket $TARGET_BUCKET --dir ./output
