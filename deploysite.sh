b10a3ec2c2b08a12c98052a27629adabc01012eb
adamkrieger <adamkrieger.ca@gmail.com>
2017-02-09 13:51:30 -0600

    Fixed AWS access key env var name.

--- deploysite.sh
+++ deploysite.sh
@@ -1,6 +1,6 @@
 HERE=`pwd`
 
 docker run -v $HERE:/usr/vol --rm --name boto3_instance \
-    -e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     akrieger/boto3 python /usr/src/upload_files.py --bucket $TARGET_BUCKET --dir ./output
